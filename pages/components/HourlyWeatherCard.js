cc10bf8fce0e228082b3906bc469ac5766986e8e
Max Ognenoi <maksymohnenyi>
2023-11-28 23:49:58 -0500

    some adjacetmentd

--- pages/components/HourlyWeatherCard.js
+++ pages/components/HourlyWeatherCard.js
@@ -1,40 +1,38 @@
-
-import React from "react";
-import moment from "moment";
-import { WiDaySunny, WiRain, WiCloudy, WiDayCloudy } from "react-icons/wi";
-import "../styles/Weather.css";
+// components/HourlyWeatherCard.js
+import React from 'react';
+import moment from 'moment';
+import { WiDaySunny, WiRain, WiCloudy, WiDayCloudy } from 'react-icons/wi';
+import '../styles/Weather.css';
 
 const getWeatherIcon = (weatherDescription) => {
-    switch (weatherDescription) {
-        case "Clear":
-            return <WiDaySunny />;
-        case "Rain":
-            return <WiRain />;
-        case "Clouds":
-            return <WiCloudy />;
-        default:
-            return <WiDayCloudy />;
-    };
+  switch (weatherDescription) {
+    case 'Clear':
+      return <WiDaySunny />;
+    case 'Rain':
+      return <WiRain />;
+    case 'Clouds':
+      return <WiCloudy />;
+    default:
+      return <WiDayCloudy />;
+  }
 };
 
 const HourlyWeatherCard = ({ weatherData, showTime }) => {
-    const { name, main, weather } = weatherData;
+  const { name, main, weather } = weatherData;
 
-    return (
-        <div className="weather-card">
-            <h2>{name}</h2>
-            <div className="weather-info">
-                {showTime && <div className="time-info"> {moment.unix(weatherData.dt).format("HH:mm")}</div>}
-                <div className="icon">{getWeatherIcon(weather[0].main)}</div>
-                <div className="temperature hourly-temperature">
-                    {Math.round(main.temp)}°C
-                </div>
-            </div>
-            <div className="weather-clouds">
-                <div className="clouds">{weather[0].description}</div>
-            </div>
-        </div>
-    );
+  return (
+    <div className="weather-card">
+      <h2>{name}</h2>
+      <div className="weather-info">
+        {showTime && <div className="time-info"> {moment.unix(weatherData.dt).format('HH:mm')}</div>}
+        <div className="icon">{getWeatherIcon(weather[0].main)}</div>
+        <div className="temperature hourly-temperature">{Math.round(main.temp)}°C</div>
+      </div>
+      <div className="weather-clouds">
+        <div className="clouds">{weather[0].description}</div>
+      </div>
+    </div>
+  );
 };
 
 export default HourlyWeatherCard;
---
+++ pages/components/index.js
@@ -0,0 +1,14 @@
+// pages/index.js
+import React from 'react';
+import Weather from './Weather';
+
+function Home() {
+  return (
+    <div>
+      <h1>Your Next.js App</h1>
+      <Weather />
+    </div>
+  );
+}
+
+export default Home;
